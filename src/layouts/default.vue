e40eaec36c81be84958dda30c608b4ea72b956f4
Zen <muhzaini30@gmail.com>
2022-06-19 12:57:12 +0800

    style: prettier

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,8 +1,13 @@
 <template>
-	<div class="p-4">
-		<router-view></router-view>
-		<div class="mx-auto w-min block mt-3">
-			<a href="https://ko-fi.com/mzaini30" style='font-size:10px' class="inline-block border hover:bg-black hover:text-white px-3 py-1">Sponsor</a>
-		</div>
-	</div>
+  <div class="p-4">
+    <router-view></router-view>
+    <div class="mx-auto w-min block mt-3">
+      <a
+        href="https://ko-fi.com/mzaini30"
+        style="font-size: 10px"
+        class="inline-block border hover:bg-black hover:text-white px-3 py-1"
+        >Sponsor</a
+      >
+    </div>
+  </div>
 </template>
