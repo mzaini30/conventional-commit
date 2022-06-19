f9472df19069c9dc7a81395c0ce6e4cb6fb17e36
Zen <muhzaini30@gmail.com>
2022-06-19 12:56:34 +0800

    feat: done

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,3 +1,8 @@
 <template>
-	<router-view></router-view>
+	<div class="p-4">
+		<router-view></router-view>
+		<div class="mx-auto w-min block mt-3">
+			<a href="https://ko-fi.com/mzaini30" style='font-size:10px' class="inline-block border hover:bg-black hover:text-white px-3 py-1">Sponsor</a>
+		</div>
+	</div>
 </template>
---
+++ src/pages/index.md
@@ -0,0 +1,23 @@
+feat: 🚀 Enhancements
+
+perf: 🔥 Performance
+
+fix: 🩹 Fixes
+
+refactor: 💅 Refactors
+
+examples: 🏀 Examples
+
+docs: 📖 Documentation
+
+chore: 🏡 Chore
+
+build: 📦 Build
+
+test: ✅ Tests
+
+types: 🌊 Types
+
+style: 🎨 Styles
+
+ci: 🤖 CI
